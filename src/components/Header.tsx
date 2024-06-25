6f2cac630e9d9ba2bec3e1a2b7c766cbde6cf262
Lucas Donato <lucasdonato2000@gmail.com>
2024-06-25 00:55:11 -0300

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,50 +21,61 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="bg-black shadow-md">
-      <div className="container mx-auto bg-white rounded-3xl flex justify-between items-center p-2 mt-8">
-        <div className="ml-5 text-lg font-bold text-gray-800 pl-3">
+    <header className="bg-black shadow-md  hidden lg:flex">
+      <div className="container mx-auto bg-white rounded-3xl flex justify-between items-center h-16 p-4 mt-8 relative">
+        <div className="ml-5 text-lg font-bold text-gray-800 pl-3 pr-3">
           <a href="/">
             <Image
               src="/Logo.svg"
               alt="buildonline logo"
               width={125}
               height={50}
+              className="sm:w-32 sm:h-12"
             />
           </a>
         </div>
-        <div className="flex-1 flex justify-end">
+        <div className="absolute left-1/2 transform -translate-x-1/2 hidden sm:flex">
           <nav className="flex space-x-4 font-medium font-inter gradient-text">
-            <a href="/contactDashboard" className="p-4 mr-2">
+            <a href="/contactDashboard" className="p-2 sm:p-4 mr-2">
               Contacts
             </a>
-          </nav>
-        </div>
-        <div className="flex-1 flex justify-start mr-auto">
-          <nav className="flex space-x-4 font-medium font-inter gradient-text">
-            <a href="/noteDashboard" className="p-4 ml-10">
+            <a href="/noteDashboard" className="p-2 sm:p-4 ml-2">
               Notes
             </a>
           </nav>
         </div>
-        <div className="flex flex-none justify-end">
+        <div
+          className="flex flex-none justify-end items-center"
+          style={{ minWidth: "250px" }}
+        >
           <nav className="flex space-x-4 font-medium items-center font-inter pr-3">
             {isAuthenticated ? (
               <>
-                <span className="text-black-600 p-4">Hola, {user?.email}</span>
+                <span
+                  className="text-black-600 p-2 sm:p-4 truncate"
+                  style={{ maxWidth: "200px" }}
+                >
+                  Hola, {user?.email}
+                </span>
                 <Button
                   onClick={handleLogout}
-                  className="py-0.5 px-6 leading-9"
+                  className="py-1 px-4 sm:py-0.5 sm:px-6 leading-9"
                 >
                   Log out
                 </Button>
               </>
             ) : (
               <>
-                <a href="/" className="text-black-600 p-4 hover:text-black-800">
+                <a
+                  href="/"
+                  className="text-black-600 p-2 sm:p-4 hover:text-black-800"
+                >
                   Log in
                 </a>
-                <Button onClick={() => {}} className="py-0.5 px-6 leading-9">
+                <Button
+                  onClick={() => {}}
+                  className="py-1 px-4 sm:py-0.5 sm:px-6 leading-9"
+                >
                   Sign in
                 </Button>
               </>
